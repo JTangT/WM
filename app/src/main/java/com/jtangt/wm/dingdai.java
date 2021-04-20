4351f982248b6fe6e148d1b54438a88d0b905443
JTangT <ttj2350549626@gmail.com>
2021-04-20 15:51:34 +0800

    update

---
+++ app/src/main/java/com/jtangt/wm/dingdai.java
@@ -0,0 +1,14 @@
+package com.jtangt.wm;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class dingdai extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dingdai);
+    }
+}
